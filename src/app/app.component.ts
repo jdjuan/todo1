910cad0885c6755c9bc58cc96ed749bb0ce1cdb3
jdjuan <david.juanherrera@gmail.com>
2018-05-02 19:12:55 -0500

    add person information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Person {
+  name: string;
+  age: number;
+  twitter: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  person: Person = {
+    name: 'Juan',
+    age: 24,
+    twitter: '@jdjuan',
+  };
+
+  constructor() {}
 }
